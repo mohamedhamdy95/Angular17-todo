da427f2010388aeb97fbf31edbd78b8aa7c6ac3a
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-04-05 14:40:16 +0200

    5/4

--- src/app/components/toda/toda.component.ts
+++ src/app/components/toda/toda.component.ts
@@ -4,25 +4,44 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-toda',
   standalone: true,
-  imports: [
-    FormsModule
-  ],
+  imports: [FormsModule],
   templateUrl: './toda.component.html',
   styleUrl: './toda.component.scss',
 })
 export class TodaComponent {
-  title: string = 'To Do List App';
+  title: string = 'To Do List App - Angular17';
   imageLink: string = 'https://cdn-icons-png.flaticon.com/512/4697/4697260.png';
   tasksList: any[] = [];
-  nawTaskItem:string='';
-  addTask(){
-    if(this.nawTaskItem !== null){
-      this.tasksList.push(this.nawTaskItem)
+  nawTaskItem: string = '';
+  showTable: boolean = false;
+  addTask() {
+    if (this.nawTaskItem.trim() !== '') {
+      this.tasksList.push(this.nawTaskItem);
+      this.nawTaskItem = '';
+      this.showTable = true;
+    } else {
+      alert('enter your item');
     }
-    console.log(this.nawTaskItem)
-    console.log(this.tasksList)
-    };
+    console.log(this.nawTaskItem);
+    console.log(this.tasksList);
+  }
+  remove(index: number) {
+    this.tasksList.splice(index, 1);
+    this.showTable = this.tasksList.length > 0;
+  }
+
+  // edit(index: number) {
+  //   let updatedTask = prompt("Edit Your Task",this.tasksList[index])
+  //   this.tasksList[index] = updatedTask?.trim()
+  // }
+edit(index:number,updatedTask:string) : string | void{
+const trimedTask = updatedTask.trim();
+if(trimedTask !== ''){
+this.tasksList[index] = trimedTask;
+}else{
+  updatedTask = this.tasksList[index]
+  return this.nawTaskItem = updatedTask;
 }
-
-
-
+this.nawTaskItem = ""
+}
+}
